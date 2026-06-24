556afe1c75ad50a18c09846157fe2b8831e6c567
yumiaowu <yumiaowu@vip.qq.com>
2026-06-24 16:06:57 +0800

    Rebuild with proper module scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react()],
   base: './',
-  build: {
-    cssCodeSplit: false,
-    assetsInlineLimit: 1024 * 1024,
-  },
 })
